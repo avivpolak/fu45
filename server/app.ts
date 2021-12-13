9393a34d6b59a42d3f04270c308f867c00033de3
Noam Golani <noam.golani@gmail.com>
2021-12-14 01:43:33 +0200

    more integration

--- server/app.ts
+++ server/app.ts
@@ -3,7 +3,7 @@ import morgan from 'morgan';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-import {MONGO_URI} from './utils/config';
+import {MONGO_URI, NODE_ENV} from './utils/config';
 
 import blogsRouter from './routers/blogsRouter';
 
@@ -20,7 +20,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(morgan('dev'));
+if (NODE_ENV !== 'test') app.use(morgan('dev'));
 
 app.use('/api/blogs', blogsRouter);
 
--- server/tests/blogApi.test.ts
+++ server/tests/blogApi.test.ts
@@ -3,17 +3,19 @@ import mongoose from 'mongoose';
 
 import {BlogData} from '../types/index';
 
+import {mockBlogs, newBlog} from './mocks';
+
 import Blog from '../models/Blogs';
 import app from '../app';
 
 const api = supertest(app);
 
-const mockBlogsData: BlogData[] = [];
-
 beforeAll(async () => {
   await Blog.deleteMany({});
-  await Blog.bulkWrite(mockBlogsData);
-});
+  await Blog.insertMany(mockBlogs);
+}, 30000);
+
+let blogId: string;
 
 describe('GET /api/blogs', () => {
   test('blogs are returned as json', async () => {
@@ -21,6 +23,44 @@ describe('GET /api/blogs', () => {
       .get('/api/blogs')
       .expect(200)
       .expect('Content-Type', /application\/json/);
+  });
+  test('all the blogs are returned', async () => {
+    const {body} = await api.get('/api/blogs');
+    blogId = body[0].id;
+    expect(body.length).toBe(mockBlogs.length);
+  });
+});
+
+describe('GET /api/blogs/:id', () => {
+  test('single blog is returned as json', async () => {
+    await api
+      .get(`/api/blogs/${blogId}`)
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  });
+
+  test('blog is returned with id and not _id or __v', async () => {
+    const {body: blog} = await api.get(`/api/blogs/${blogId}`);
+    expect(blog.id).toBeDefined();
+    expect(blog._id).not.toBeDefined();
+    expect(blog.__v).not.toBeDefined();
+  });
+});
+
+describe('POST /api/blogs/', () => {
+  const beforeLength = mockBlogs.length;
+  test('single blog is returned as json and save correctly', async () => {
+    const {body: blog} = await api
+      .post(`/api/blogs/`)
+      .send(newBlog)
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+
+    const {body: blogs} = await api.get(`/api/blogs/`);
+    expect(blogs.length).toBe(beforeLength + 1);
+
+    delete blog.id;
+    expect(blog).toEqual(newBlog);
   });
 });
 
