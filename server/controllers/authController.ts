e286188548a1b628221d01884b3b2ff86714cde4
Noam Golani <noam.golani@gmail.com>
2021-12-14 13:54:35 +0200

    auth route controller

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -1,17 +1,35 @@
 import {Handler} from 'express';
-import {isValidObjectId} from 'mongoose';
+import bcrypt from 'bcrypt';
+
+import {HASH_SALT} from '../utils/config';
+
+import {LoginRequest, RegisterRequest} from '../types/index';
 
 import User from '../models/Users';
 
-export const login: Handler = async (req, res, next) => {
+export const login: Handler = async (req: LoginRequest, res, next) => {
   try {
+    if (!req.validated) throw Error('No validated obj');
+    const {username, password} = req.validated;
+    const user = await User.findOne({username});
+    if (!user) res.status(400).send('No such username');
+    if (!(await bcrypt.compare(password, user.password)))
+      res.status(400).send('Bad password');
   } catch (error) {
     next(error);
   }
 };
 
-export const register: Handler = async (req, res, next) => {
+export const register: Handler = async (req: RegisterRequest, res, next) => {
   try {
+    if (!req.validated) throw Error('No validated obj');
+    const {name, username, password} = req.validated;
+    const exists = await User.findOne({username});
+    if (exists) res.status(400).send('User already exists');
+
+    const hashedPassword = bcrypt.hash(password, HASH_SALT);
+    const user = await User.create({username, name, password: hashedPassword});
+    res.send(user);
   } catch (error) {
     next(error);
   }
--- server/utils/config.ts
+++ server/utils/config.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const PORT = process.env.PORT || 8080;
+export const HASH_SALT = process.env.HASH_SALT || 'salt';
 export const NODE_ENV = process.env.NODE_ENV;
 export const MONGO_URI =
   NODE_ENV === 'test' ? process.env.TEST_MONGO_URI : process.env.MONGO_URI;
